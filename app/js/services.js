4b4db9637f3fab4ed69601d572b5467f123d3e71
MoMenne <mpmenne@electronicsystemssupport.com>
2014-05-14 16:03:16 -0500

    removing socket.io because its cached

--- app/js/services.js
+++ app/js/services.js
@@ -435,24 +435,24 @@ snapscripApp.factory('PdfService', function($http, $q, $filter) {
 
 snapscripApp.
     factory('socket', function($rootScope) {
-      var socket = io.connect('/socket.io');
+//      var socket = io.connect('/socket.io');
       return {
         on : function(eventName, callback) {
-          socket.on(eventName, function() {
-            var args = arguments;
-            $rootScope.$apply(function() {
-              callback.apply(socket, args);
-            });
-          });
+//          socket.on(eventName, function() {
+//            var args = arguments;
+//            $rootScope.$apply(function() {
+//              callback.apply(socket, args);
+//            });
+//          });
         },
         emit : function(eventName, data, callback) {
           socket.emit(eventName, data, function() {
-            var args = arguments;
-            $rootScope.$apply(function() {
-              if (callback) {
-                callback.apply(socket, args);
-              }
-            })
+//            var args = arguments;
+//            $rootScope.$apply(function() {
+//              if (callback) {
+//                callback.apply(socket, args);
+//              }
+//            })
           })
         }
       }
